d7029c15ef4ce2a3a11c2ad9873b27ae14451790
Juan Manuel Paredes <139470808+juanparedes26@users.noreply.github.com>
2025-02-04 00:17:36 +0000

    termine

--- src/app.js
+++ src/app.js
@@ -7,9 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  
+  let arrayColores=["red","black"];
+  let randomColores=arrayColores[Math.floor(Math.random()*arrayColores.length)]
+
   let arraySimbolos=["♦", "♥", "♠", "♣"];
   let randomSimbolo=arraySimbolos[Math.floor(Math.random()*arraySimbolos.length)]
-  document.getElementById("simbolo11").innerHTML=randomSimbolo;
-  document.getElementById("simbolo22").innerHTML=randomSimbolo;
+
+  let color;
+  if (randomSimbolo === "♠" || randomSimbolo === "♣") {
+      color = "black"; 
+  } else if (randomSimbolo === "♥" || randomSimbolo === "♦") {
+      color = "red"; 
+  }
+
+  document.getElementById("simbolo11").innerHTML = randomSimbolo;
+  document.getElementById("simbolo11").style.color = color; // Establece el color
+  document.getElementById("simbolo22").innerHTML = randomSimbolo;
+  document.getElementById("simbolo22").style.color = color; // Establece el color
+  
+
+  let arrayNumero=[ 2,3,4,5,6,7,8,9,"A","J","Q","K"]
+  let randomNumero=arrayNumero[Math.floor(Math.random()*arrayNumero.length)]
+  document.getElementById("numeroRandom").innerHTML=randomNumero;
 
 };
